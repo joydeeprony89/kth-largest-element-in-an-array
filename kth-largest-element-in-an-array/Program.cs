9a8add12d15b32478b5eaf8c7a8df66311957183
Jaydeep Shil <ED8A@eurofins.com>
2021-08-24 21:49:32 +0530

    added another partititon sample

--- kth-largest-element-in-an-array/Program.cs
+++ kth-largest-element-in-an-array/Program.cs
@@ -7,17 +7,21 @@ namespace kth_largest_element_in_an_array
         static void Main(string[] args)
         {
             int[] arr = { 0, 1 , -1, -2, 9 };
-            int k = 5;
-            Console.Write("K'th largest element is " + FindKthLargest(arr, k));
+            Console.WriteLine($"Input is - {string.Join(",", arr)}");
+            int k = 3;
+            Console.Write($"{k}'th largest element is " + FindKthLargest(arr, k));
         }
 
         static int FindKthLargest(int[] nums, int k)
         {
-            k = nums.Length - k;
+            k = nums.Length - k; // k'th largest is similar to (length - k) th index element from an asc sorted array.
             int l = 0, r = nums.Length - 1;
             while (l < r)
             {
                 int position = Partition(nums, l, r);
+                // similar to quick sort except the below check
+                // we take the partition ( from partition position left side elements are lesser , right side elements are greater of partition elemenet)
+                // based on the value of K we update the left / right pointer to carry our sorting for the required portion, instead of entire array to be sorted.
                 if (k < position)
                 {
                     r = position - 1;
@@ -45,6 +49,23 @@ namespace kth_largest_element_in_an_array
             }
             Swap(nums, i, r);
             return i;
+        }
+
+        static int Another_Partition(int[] nums, int l, int r)
+        {
+            int i = l - 1;
+            int pivot = nums[r];
+            for (int j = l; j <= r - 1; j++)
+            {
+                if (nums[j] < pivot)
+                {
+                    i++;
+                    Swap(nums, i, j);
+                }
+            }
+
+            Swap(nums, i + 1, r);
+            return i + 1;
         }
 
         static void Swap(int[] nums, int i, int j)
